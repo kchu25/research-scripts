2cdded83096133ae38477ad499c8e02f68255208
kchu25 <skchu@wustl.edu>
2024-08-14 13:23:34 -0400

    update hash function

--- universal_hash_functions.jl
+++ universal_hash_functions.jl
@@ -8,35 +8,32 @@ ref: https://www.cs.cmu.edu/~avrim/451f11/lectures/lect1004.pdf 10.6.1
 
 
 Create a universal hash function 
-    h: Z₊ → Z₊ 
+    h: (Z₊)ⁿ → Z₊ 
     (Z₊ is the set of positive integers)
+    that takes a vector x = (x₁, x₂, ..., xₙ) as input
+    and returns a hash value h(x) in the range {0,1,...,M-1}
+    where M is a prime number    
 Input:
-    K: upper bound of the input (positive integers)
+    K: size (dimension) of the input (positive integers)
     M: a large enough prime number
         (large enough to avoid collisions)
+    upperbdd: upper bound for each random input component xᵢ
+        (default: 100)
 Output:
     h: a universal hash function
         h(x) = (∑ᵢ rᵢ xᵢ) mod M
         where rᵢ are random numbers in {0,1,...,M-1}
 =#
 
-function create_universal_hash(K::Int, M::Int)
-    @assert isprime(M) "M must be a prime number"
+function create_universal_hash(K::Int; upperbdd = 100)
+    M = nextprime(upperbdd^K)
     r = [rand(0:M-1) for _ = 1:K]
     h(x) = sum(r .* x) % M
     return h
 end
 
-K= 100
-h = create_universal_hash(K,991)
+K= 5
+h = create_universal_hash(K)
 
-for i = 1:K
-    println(h(i))
-end
-
-[h(i) for i = 1:K] |> unique |> length
-
-Vector
-
-
-hs = [create_universal_hash(K,59) for _ = 1:5]
+h([1,2,3,4,5])
+h([3,4,5,6,7])
